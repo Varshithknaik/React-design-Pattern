f397da6de25766d7b3c17d11754d7eb56a44a581
Varshith K <varshithknaik@gmail.com>
2025-04-13 10:53:49 +0530

    Refactor Main component to use Suspense and Await for data fetching; update loader delay to 2000ms

--- advanced-concepts-and-hooks/hooks/src/components/async-components/main.tsx
+++ advanced-concepts-and-hooks/hooks/src/components/async-components/main.tsx
@@ -1,19 +1,25 @@
-import { useLoaderData } from "react-router";
+import { Await, useLoaderData } from "react-router";
 import { MainContainer, MainHeading } from "./styled-elements";
 import delay from "../util/delay";
+import { Suspense } from "react";
 
 const Main = () => {
   const data = useLoaderData();
 
   return (
     <MainContainer>
-      <MainHeading>Main - {data}</MainHeading>
+      <MainHeading>
+        Main -
+        <Suspense fallback={<p>Fetching...</p>}>
+          <Await resolve={data}>{data}</Await>
+        </Suspense>
+      </MainHeading>
     </MainContainer>
   );
 };
 
 async function loader() {
-  return await delay("Fetched Data", 1000);
+  return await delay("Fetched Data", 2000);
 }
 
 export const mainRoute = { element: <Main />, loader };
